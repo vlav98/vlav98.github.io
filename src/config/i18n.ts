1b7c5caa45af9117202c583dba4751e4cd88bbc8
Valentine Lav <valentinelav98@gmail.com>
2023-12-09 12:25:34 +0100

    ✨ Implement translation

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,4 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import commonJson from "../locales/en/common.json";
+import commonFrJson from "../locales/fr/common.json";
 
-i18n.use(initReactI18next);
+const resources = {
+  en: {
+    common: commonJson,
+  },
+  fr: {
+    common: commonFrJson,
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({ resources, lng: "en", interpolation: { escapeValue: false } });
+
+export default i18n;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
+import { useTranslation } from "react-i18next";
 
 const Home: React.FC = () => {
+  const { t } = useTranslation("common");
+
   return (
     <div className="flex-1">
-      <Hero title={"Vlav98"} subtitle={"Coding makes me cry"}></Hero>
+      <Hero title={t("title")} subtitle={t("subtitle")}></Hero>
     </div>
   );
 };
